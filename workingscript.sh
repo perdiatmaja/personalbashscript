772034f5947358c0c133117b806fb67c1fa08c5b
Perdi Atmaja <perdi.atmaja@dana.id>
2019-11-20 13:59:25 +0700

    Add personal working script

---
+++ workingscript.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+getCase (){
+    case "$1" in
+        i)
+        echo "improvement";;
+        b)
+        echo "bugfix";;
+        f)
+        echo "feature";;
+    esac
+}
+
+yamete () { git checkout -b "$(getCase $1)"/SAND2"-""$2"; }
+kimochi () { git checkout "$(getCase $1)"/SAND2"-""$2"; }
+senpai () {git checkout release/"$1"}
+
+alias dameeee="git reset --hard";
+alias c="clear";
+alias minyakUrut="git pull";
+alias ahShitHereWeGoAgain="git checkout develop";
+alias tehTarik="git fetch";
+
+if [ $PWD = '/Users/'$(whoami) ]
+then
+    echo "Back to work?"
+    read yn
+    case $yn in
+        y|Y|yes|YES)
+        cd ~/Documents/Project/Android/skywalker_artoodetoo_android/;;
+    esac
+fi
